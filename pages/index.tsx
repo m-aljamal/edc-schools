edd9c0f003a906c339b5e283221a400e644474e6
m-aljamal <mohammadjamol@gmail.com>
2021-02-13 23:58:13 +0300

    add revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,7 @@ export async function getStaticProps({ preview = false }) {
   const allHeroImages = await getAllImages(preview);
   return {
     props: { allHeroImages, preview },
+    revalidate: 5,
   };
 }
 
