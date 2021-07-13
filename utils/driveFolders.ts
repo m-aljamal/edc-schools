d6279e0449881f470715d5986b2cccb5fd6296fd
Mohammad Aljamal <m.aljamal@edcommission.com>
2021-07-13 13:26:51 +0300

    remove files

--- utils/driveFolders.ts
+++ utils/driveFolders.ts
@@ -3,23 +3,23 @@ const wordType = "application/vnd.google-apps.document"
 export const teacherFolders = [
     {
         mainFolder: "إثراء المنهاج",
-        file: "إثراء المناهج.docx",
-        mimeType:  wordType
+        // file: "إثراء المناهج.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "الخطة الدرسية",
-        file:"التقويم الدراسي.xlsx",
-        mimeType: excelType ,
+        // file:"التقويم الدراسي.xlsx",
+        // mimeType: excelType ,
         subFolders:[
             {
                 folder: "الخطة الدرسية الفصلية",
-                file: "خطة توزيع المنهج للفصل الدراسي.docx",
-                mimeType:  wordType
+                // file: "خطة توزيع المنهج للفصل الدراسي.docx",
+                // mimeType:  wordType
             },
             {
                 folder:"الخطة الدرسية الأسبوعية", 
-                file:"الخطة الأسبوعية للمعلم.xlsx",
-                mimeType: excelType ,
+                // file:"الخطة الأسبوعية للمعلم.xlsx",
+                // mimeType: excelType ,
             }
         ],
     },
@@ -39,79 +39,79 @@ export const teacherFolders = [
     },
     {
         mainFolder: "تحضير الدروس",
-        file: "دفتر تحضير.docx",
-        mimeType:  wordType
+        // file: "دفتر تحضير.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "تحليل نتائج الاختبارات",
-        file: "تحليل نتائج الاختبارات.docx",
-        mimeType:  wordType
+        // file: "تحليل نتائج الاختبارات.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "تنمية المهارات والمواهب",
-        file: "تنمية المهارات والمواهب.docx",
-        mimeType:  wordType
+        // file: "تنمية المهارات والمواهب.docx",
+        // mimeType:  wordType
     },
 ]
 export const manger = [
     {
         mainFolder: "إذن إنصراف",
-        file: "إذن بالانصراف.docx",
-        mimeType:  wordType
+        // file: "إذن بالانصراف.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "استجواب",
-        file: "استجواب.docx",
-        mimeType:  wordType
+        // file: "استجواب.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "استئذان موظف",
-        file: "استئذان موظف.docx",
-        mimeType:  wordType
+        // file: "استئذان موظف.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "إقالة موظف",
-        file: "استئذان موظف.docx",
-        mimeType:  wordType
+        // file: "استئذان موظف.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "التقويم الدراسي وجدول الفعاليات",
-        file: "التقويم الدراسي وجدول الفعاليات.xlsx",
-        mimeType:  excelType
+        // file: "التقويم الدراسي وجدول الفعاليات.xlsx",
+        // mimeType:  excelType
     },
     {
         mainFolder: "انفكاك موظف",
-        file: "انفكاك موظف.docx",
-        mimeType:  wordType
+        // file: "انفكاك موظف.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "تعهد ولي أمر",
-        file: "تعهد ولي أمر.docx",
-        mimeType:  wordType
+        // file: "تعهد ولي أمر.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "تقرير الإنجازات التربوية والأنشطة المدرسية",
-        file: "تقرير الإنجازات التربوية والأنشطة المدرسية.docx",
-        mimeType:  wordType
+        // file: "تقرير الإنجازات التربوية والأنشطة المدرسية.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "تنبيه للمعلم كتابي",
-        file: "تنبيه للمعلم كتابي.docx",
-        mimeType:  wordType
+        // file: "تنبيه للمعلم كتابي.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "قرار فصل طالب",
-        file: "قرار فصل طالب.docx",
-        mimeType:  wordType
+        // file: "قرار فصل طالب.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "مباشرة عمل",
-        file: "مباشرة عمل.docx",
-        mimeType:  wordType
+        // file: "مباشرة عمل.docx",
+        // mimeType:  wordType
     },
     {
         mainFolder: "مكافات خاصة",
-        file: "مكافات خاصة.docx",
-        mimeType:  wordType
+        // file: "مكافات خاصة.docx",
+        // mimeType:  wordType
     },
 ]
